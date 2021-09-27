3ffc2967c58f5fd9aa199bf46b1efb6e2c3891b3
natemitch11 <nate.mitch11@gmail.com>
2021-09-27 15:28:52 -0600

    added 2nd unit test

--- project.test.js
+++ project.test.js
@@ -3,3 +3,7 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test('can create a basic greeting', ()=> {
+  expect(getGreeting('Bob')).toBe("Hi Bob");
+});
